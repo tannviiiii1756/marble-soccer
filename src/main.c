5329d81dfab0fd68cdbc2c40fa498cfdb85180df
tannviiiii1756 <tanvitoocool170506@gmail.com>
2025-11-20 00:54:06 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,24 +1,39 @@
 #include <SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <time.h>
 #include "render.h"
 #include "game.h"
 
 int run_render();
 int run_no_render();
+int run_baseline_agent();   // <-- NEW
 
 int main(int argc, char *argv[])
 {
     bool render = false;
+    bool baseline_agent = false;   // <-- NEW
+
     for (int i = 1; i < argc; i++)
     {
         if (strcmp(argv[i], "--render") == 0)
         {
             render = true;
         }
+        else if (strcmp(argv[i], "--agent") == 0 && i + 1 < argc)
+        {
+            if (strcmp(argv[i + 1], "baseline") == 0)
+            {
+                baseline_agent = true;
+            }
+        }
     }
 
     srand((unsigned int)time(NULL));
+
+    if (baseline_agent)
+        return run_baseline_agent();
 
     return render ? run_render() : run_no_render();
 }
@@ -68,5 +83,13 @@ int run_render()
 int run_no_render()
 {
     game_play_no_render();
+    return 0;
+}
+
+// NEW FUNCTION
+int run_baseline_agent()
+{
+    printf("Running baseline agent simulation...\n");
+    game_play_baseline_agent();   // <-- You will implement this in game.c
     return 0;
 }
